39e9e6717a340f2eddaca4b9b59c130947fb0590
Kate Nelson <mkatenelson@gmail.com>
2016-08-30 17:17:53 -0400

    'sort' property undefined error

--- js/app.js
+++ js/app.js
@@ -1,50 +1,12 @@
-var NavigationItem = React.createClass({
-  onClick: function() {
-    this.props.itemSelected(this.props.item);
-  },
-  render: function() {
-    return (
-      <li onClick={this.onClick} className={this.props.selected ? "selected" : ""}>
-        {this.props.item.data.display_name}
-      </li>
-    );
-  }
-});
-
-var Navigation = React.createClass({
-  setSelectedItem: function(item) {
-    this.props.itemSelected(item);
-  },
+var App = React.createClass({
   render: function() {
     var items = this.props.items
       .sort(function(a, b) {
         // Sort by # of subscribers in descending order
         return b.data.subscribers - a.data.subscribers;
-      })
-      .map(function(item) {
-        return (
-          <NavigationItem
-            item={item}
-            itemSelected={this.setSelectedItem}
-            key={item.data.id}
-            selected={item.data.url === this.props.activeUrl} />
-        );
-      }, this);
+      });
 
-    return (
-      <div className="navigation">
-        <div className="header">Navigation</div>
-        <ul>
-            {items}
-        </ul>
-      </div>
-    );
-  }
-});
-
-var StoryList = React.createClass({
-  render: function() {
-    var storyNodes = this.props.items.map(function(item) {
+    var storyNodes = this.props.items.sort(function(item) {
       return (
         <tr key={item.data.id}>
           <td>
@@ -71,64 +33,6 @@ var StoryList = React.createClass({
         </tbody>
       </table>
     );
-  }
-});
-
-var App = React.createClass({
-  componentDidMount: function() {
-    var _this = this;
-    var cbname = "fn" + Date.now();
-    var script = document.createElement("script");
-    script.src = "http://www.reddit.com/reddits.json?jsonp=" + cbname;
-
-    window[cbname] = function(jsonData) {
-      _this.setState({
-        navigationItems: jsonData.data.children
-      });
-      delete window[cbname];
-    };
-
-    document.head.appendChild(script);
-  },
-  getInitialState: function() {
-    return ({
-      activeNavigationUrl: "",
-      navigationItems: [],
-      storyItems: [],
-      title: "Please select a sub"
-    });
-  },
-  render: function() {
-    return (
-      <div>
-        <h1>{this.state.title}</h1>
-        <Navigation
-          activeUrl={this.state.activeNavigationUrl}
-          items={this.state.navigationItems}
-          itemSelected={this.setSelectedItem} />
-        <StoryList items={this.state.storyItems} />
-      </div>
-    );
-  },
-  setSelectedItem: function(item) {
-    var _this = this;
-    var cbname = "fn" + Date.now();
-    var script = document.createElement("script");
-    script.src = "http://www.reddit.com/" + item.data.url +
-      ".json?sort=top&t=month&jsonp=" + cbname;
-
-    window[cbname] = function(jsonData) {
-      _this.setState({storyItems: jsonData.data.children});
-      delete window[cbname];
-    };
-
-    document.head.appendChild(script);
-
-    this.setState({
-      activeNavigationUrl: item.data.url,
-      storyItems: [],
-      title: item.data.display_name
-    });
   }
 });
 
